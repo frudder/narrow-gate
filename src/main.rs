bff804b1a9013f48b0dd69b152d3d225cc8c62fe
ff <vorjohnl@gmail.com>
2025-04-29 11:16:40 +0800

    feat: bytes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 
+
 #[tokio::main]
 async fn main() -> Result<(),  Box<dyn std::error::Error + Send + Sync>> {
     println!("Hello, world!");
@@ -6,13 +7,31 @@ async fn main() -> Result<(),  Box<dyn std::error::Error + Send + Sync>> {
 }
 
 
-
 #[cfg(test)]
 mod tests {
+
+    use http_body_util::Empty;
+    use hyper::Request;
+    use hyper_util::client::legacy::{connect::HttpConnector, Client};
+
 
     #[tokio::test]
     async fn foo() -> Result<(),  Box<dyn std::error::Error + Send + Sync>> {
         println!("hello world");
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn bar() -> Result<(),  Box<dyn std::error::Error + Send + Sync>> {
+        
+        let h = Client::builder(hyper_util::rt::TokioExecutor::new()).build(HttpConnector::new());
+        
+        let req = Request::builder()
+            .uri("https://www.rust-lang.org/")
+            .method("GET")
+            .body(Empty::<bytes::Bytes>::new())?;
+
+
         Ok(())
     }
 }
