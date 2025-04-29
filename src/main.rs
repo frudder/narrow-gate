556501645aa3e932e06cf238c6950c13ea3c9991
ff <vorjohnl@gmail.com>
2025-04-29 10:43:08 +0800

    feat: test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+
+#[tokio::main]
+async fn main() -> Result<(),  Box<dyn std::error::Error + Send + Sync>> {
     println!("Hello, world!");
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    #[tokio::test]
+    async fn foo() -> Result<(),  Box<dyn std::error::Error + Send + Sync>> {
+        println!("hello world");
+        Ok(())
+    }
 }
